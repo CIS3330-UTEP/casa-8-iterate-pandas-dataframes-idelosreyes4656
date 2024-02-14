3474b3d443a46bbb362979a20a9b9e2f01561c5e
idelosreyes4656 <157189340+idelosreyes4656@users.noreply.github.com>
2024-02-14 00:22:26 -0700

    Update casa_8.py
    update 1

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,22 @@
+import pandas as pd
+
+filename = "big-mac-full-index.csv"
+
+df = pd.read_csv(filename) 
+
+# print(df.columns)
+
+# Method 4: Using iterrows() method of the Dataframe. 
+for index,row in df.iterrows():
+    print(row['name'],
+          row['local_price'],
+          row['adj_price'],
+          row['USD_adjusted'],
+          row['date'],
+          row['iso_a3'])
+          
+# Method 6:  Using apply() method of the Dataframe. 
+print(df.apply(lambda row: (row['name'], row['local_price']), axis = 1))
+
+
 
